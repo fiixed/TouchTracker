2d0acc1827bbf697f4ea26b1a101d4a215046130
Andrew Bell <fiixed@gmail.com>
2015-01-08 11:13:22 +1100

    Initial Commit

---
+++ TouchTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchTracker
+//
+//  Created by Andrew Bell on 1/8/15.
+//  Copyright (c) 2015 FiixedMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
